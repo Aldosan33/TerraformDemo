55b00bfe363e917d2ab33a00d647855db4bba7a5
Alfredo Sanchez Alvarez <alfredo.2993@gmail.com>
2021-01-07 00:20:02 -0500

    Update pipeline files

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf_rg_blobstorage"
+    storage_account_name = "tfstorageaccountaldosan"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "tf_resource_group" {
   name     = "tfmainrg"
   location = "Brazil South"
